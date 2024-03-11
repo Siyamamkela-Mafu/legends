c1f61f35001184cd1243ee7928cd90fe13192d04
Siyama Mafu <siyama.mafu@entelect.co.za>
2024-03-11 13:41:12 +0200

    Add annotation

--- src/main/java/com/siyama/legends/controller/RequiredItemsController.java
+++ src/main/java/com/siyama/legends/controller/RequiredItemsController.java
@@ -2,17 +2,14 @@ package com.siyama.legends.controller;
 
 import com.siyama.legends.dtos.Item;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("required-items/")
 public class RequiredItemsController {
     @PostMapping(value = "{eventId}",
             consumes = MediaType.APPLICATION_JSON_VALUE)
-    public String addItem(@PathVariable("eventId") String eventId,Item item){
+    public String addItem(@PathVariable("eventId") String eventId, @RequestBody Item item){
 
         return item + " for " +  eventId + " saved successfully";
     }
