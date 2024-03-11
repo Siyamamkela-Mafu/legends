1dbbe6e4e5a3d8a07c01fdf470daad555cb4871a
Siyama Mafu <siyama.mafu@entelect.co.za>
2024-03-11 13:38:18 +0200

    Add path variable and media type consumed

--- src/main/java/com/siyama/legends/controller/GreetingsController.java
+++ src/main/java/com/siyama/legends/controller/GreetingsController.java
@@ -6,14 +6,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/greeting/")
+@RequestMapping("/api/greeting")
 public class GreetingsController {
     @GetMapping("{message}")
     public String getGreeting(@PathVariable("message") String message){
         return message;
     }
 
-    @GetMapping("")
+    @GetMapping()
     public String hello(){
         return "Hello user";
     }
--- src/main/java/com/siyama/legends/controller/RequiredItemsController.java
+++ src/main/java/com/siyama/legends/controller/RequiredItemsController.java
@@ -1,14 +1,19 @@
 package com.siyama.legends.controller;
 
 import com.siyama.legends.dtos.Item;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("required-items/")
+@RestController
+@RequestMapping("required-items/")
 public class RequiredItemsController {
-    @PostMapping("{eventId}/")
-    public String addItem(@PathVariable("eventId") String eventId, Item item){
-        return "Item for " +  eventId + " successfully";
+    @PostMapping(value = "{eventId}",
+            consumes = MediaType.APPLICATION_JSON_VALUE)
+    public String addItem(@PathVariable("eventId") String eventId,Item item){
+
+        return item + " for " +  eventId + " saved successfully";
     }
 }
