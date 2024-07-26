8a713cf3bfabd487f871dd282acb77ef5ce8f359
Siyama Mafu <siyama.mafu@entelect.co.za>
2024-07-26 19:13:19 +0200

    chore/code cleanup

--- src/test/java/com/siyama/legends/LegendsApplicationTests.java
+++ src/test/java/com/siyama/legends/LegendsApplicationTests.java
@@ -1,13 +1,9 @@
 package com.siyama.legends;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class LegendsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
